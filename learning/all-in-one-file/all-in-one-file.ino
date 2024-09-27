bf1f4fd35a9cee913331c0fa00cee55039a40847
Tanishq Singh <hi@tanishqsingh.com>
2024-09-28 01:30:29 +0530

    last code, but prettier

--- learning/all-in-one-file/all-in-one-file.ino
+++ learning/all-in-one-file/all-in-one-file.ino
@@ -1,10 +1,9 @@
-int baseSpeed = 100;
-int turnSpeed = 100;
-int Kp = 65;
-int IN1 = 4, IN2 = 2, ENA = 5;
-int IN3 = 7, IN4 = 3, ENB = 6;
+const int baseSpeed = 90;
+const int turnSpeed = 100;
+const int IN1 = 4, IN2 = 2, ENA = 5;
+const int IN3 = 7, IN4 = 3, ENB = 6;
 
-int sensors[5] = {A1, A2, A3, A0, A4};
+const int sensors[5] = {A1, A2, A3, A0, A4};
 String lp = "00000";
 String path = "";
 
@@ -17,12 +16,12 @@ void drive(int l, int r) {
     if (l == 0) {
         digitalWrite(IN1, 0);
         digitalWrite(IN2, 0);
-    }
+    };
 
     if (r == 0) {
         digitalWrite(IN3, 0);
         digitalWrite(IN4, 0);
-    }
+    };
 
     analogWrite(ENA, constrain(abs(l), 0, 255));
     analogWrite(ENB, constrain(abs(r), 0, 255));
@@ -53,113 +52,8 @@ void setup() {
     Serial.begin(9600);
 };
 
-void turnRight() {
-    drive(0, 0);
-    lp = readLine();
-
-    delay(200);
-
-    do { 
-        drive(turnSpeed, turnSpeed); delay(200);
-        drive(0, 0);
-
-        lp = readLine();
-    } while (lp[2] == '1' && lp[3] == '1' && lp[4] == '1');
-  
-    delay(100);
-
-    do {
-        drive(turnSpeed, -turnSpeed); delay(10);
-        drive(0, 0);
-
-        lp = readLine();
-    } while (lp[2] == '1' || lp[3] == '1');
-
-    delay(100);
-
-    do {
-        drive(turnSpeed, -turnSpeed); delay(10);
-        drive(0, 0);
-
-        lp = readLine();
-    } while(lp[2] != '1' && lp[3] != '1');
-
-    drive(0, 0);
-};
-
-void turnLeft() {
-    drive(0, 0);
-    lp = readLine();
-
-    delay(200);
-
-    do { 
-        drive(turnSpeed, turnSpeed); delay(200);
-        drive(0, 0);
-
-        lp = readLine();
-    } while (lp[0] == '1' && lp[1] == '1' && lp[2] == '1');
-  
-    delay(100);
-
-    do {
-        drive(-turnSpeed, turnSpeed); delay(10);
-        drive(0, 0);
-
-        lp = readLine();
-    } while (lp[1] == '1' || lp[2] == '1');
-
-    delay(100);
-
-    do {
-        drive(-turnSpeed, turnSpeed); delay(10);
-        drive(0, 0);
-
-        lp = readLine();
-    } while(lp[1] != '1' && lp[2] != '1');
-
-    drive(0, 0);
-
-};
-
-void overshoot() {
-    lp = readLine();
-};
-
-float tuning(String linePosition) {
-    float error;
-
-    if (linePosition == "00100") error = 0.00;
-    else if (linePosition == "01100") error = -0.25;
-    else if (linePosition == "01000") error = -0.50;
-    else if (linePosition == "11000") error = -0.75;
-    else if (linePosition == "10000") error = -1.00;
-    else if (linePosition == "00110") error =  0.25;
-    else if (linePosition == "00010") error =  0.50;
-    else if (linePosition == "00011") error =  0.75;
-    else if (linePosition == "00001") error =  1.00;
-
-    return error * Kp; 
-}
-
-void straightPID() {
-    String linePosition = readLine();
-
-    Serial.println(linePosition);
-
-    if (linePosition == "11100") turnLeft();
-    else if (linePosition == "00111") turnRight();
-    else if (linePosition == "11111") turnLeft();
-    else if (linePosition == "00000") drive(0, 0);
-    else {
-        drive(baseSpeed + tuning(linePosition), baseSpeed - tuning(linePosition));
-    };
-
-}
-
 void straight() {
     lp = readLine();
-    int s = 90; // speed
 
     while (lp != "11111" && lp != "11100" && lp != "00111") {
         if (lp[1] == '1' || lp[0] == '1') drive(0, s);
@@ -169,17 +63,15 @@ void straight() {
 
         delay(1); drive(0, 0);
         lp = readLine();
-    }
+    };
 
     delay(500);
-}
+};
 
 void oneStep(){
     drive(baseSpeed, baseSpeed); delay(150);
     drive(0, 0);
 }; 
-
-int s = 80;
 
 void turn(char t) {
     switch (t) {
@@ -234,9 +126,9 @@ void loop() {
         else {turn('R');} // Right
     }
 
-    else if (lp[1] == '1' || lp[0] == '1') drive(0, s);
-    else if (lp[3] == '1' || lp[4] == '1') drive(s, 0);
-    else if (lp[2] == '1') drive(s, s);
+    else if (lp[1] == '1' || lp[0] == '1') drive(0, baseSpeed);
+    else if (lp[3] == '1' || lp[4] == '1') drive(baseSpeed, 0);
+    else if (lp[2] == '1') drive(baseSpeed, baseSpeed);
     else {turn('U');} // u-turn
    
     delay(1);
